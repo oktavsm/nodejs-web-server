162797f9d7e52b88fda12b3a695d4fb48c3156dc
oktaavsm <incdmincs@gmail.com>
2025-12-29 15:08:16 +0700

    Refactor response handling for about page and improve error messages

--- server.js
+++ server.js
@@ -1,49 +1,57 @@
 const http = require("http");
 
 const requestListener = (request, response) => {
-  response.setHeader("Content-Type", "application/json");
-  response.setHeader("Powered-By", "Node.js");
-
-  const { method, url } = request;
-
-  if (url === "/") {
-    if (method === "GET") {
-      response.statusCode = 200;
-      response.end("<h1>Ini adalah homepage</h1>");
+    response.setHeader('Content-Type', 'application/json');
+    response.setHeader('Powered-By', 'Node.js');
+ 
+    const { method, url } = request;
+ 
+    if(url === '/') {
+        if(method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
+        }
+    } else if(url === '/about') {
+        if(method === 'GET') {
+            response.statusCode = 200;
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
+        } else if(method === 'POST') {
+            let body = [];
+    
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+ 
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }));
+            });
+        } else {
+            response.statusCode = 400;
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses menggunakan ${method}, request`
+            }));
+        }
     } else {
-      response.statusCode = 400;
-      response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
-      );
+        response.statusCode = 404;
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
     }
-  } else if (url === "/about") {
-    // TODO 3: logika respons bila url bernilai '/about'
-    if (method === "GET") {
-      response.statusCode = 200;
-      response.end("<h1>Halo! Ini adalah halaman about</h1>");
-    } else if (method === "POST") {
-      let body = [];
 
-      request.on("data", (chunk) => {
-        body.push(chunk);
-      });
-
-      request.on("end", () => {
-        body = Buffer.concat(body).toString();
-        const { name } = JSON.parse(body);
-        response.statusCode = 200;
-        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
-      });
-    } else {
-      response.statusCode = 400;
-      response.end(
-        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
-      );
-    }
-  } else {
-    response.statusCode = 404;
-    response.end("<h1>Halaman tidak ditemukan!</h1>");
-  }
 
   /** Kode komentar disembunyikan agar lebih ringkas */
 
