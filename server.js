a10ab9089c0d13e3349afec454dd920a0bd4b515
oktaavsm <incdmincs@gmail.com>
2025-12-29 14:07:44 +0700

    Implement basic HTTP server with GET and POST handling for homepage and about page

--- server.js
+++ server.js
@@ -1,1 +1,68 @@
-console.log('Halo, kita akan belajar membuat server');
+const http = require('http');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+ 
+    const { method, url } = request;
+ 
+    if(url === '/') {
+        if(method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } 
+    
+    else if(url === '/about') {
+        // TODO 3: logika respons bila url bernilai '/about'
+        if(method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>');
+        } else if(method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            })
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            })
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else {
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
+ 
+    /** Kode komentar disembunyikan agar lebih ringkas */
+
+     // if(method === 'GET') {
+    //     response.end('<h1>Hello!</h1>');
+    // }
+ 
+    // if(method === 'POST') {
+    //     let body = [];
+    
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
+ 
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         const { name } = JSON.parse(body);
+    //         response.end(`<h1>Hai, ${name}!</h2>`);
+    //     });
+    // }
+}
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
