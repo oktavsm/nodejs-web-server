ec8e28536f3c67dbbbd5defcce92769d3457ac05
oktaavsm <incdmincs@gmail.com>
2025-12-29 14:20:17 +0700

    Refactor response handling for about page and improve content type headers

--- server.js
+++ server.js
@@ -1,74 +1,76 @@
-const http = require('http');
- 
+const http = require("http");
+
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type', 'text/html');
+  response.setHeader("Content-Type", "application/json");
+  response.setHeader("Powered-By", "Node.js");
 
- 
-    const { method, url } = request;
- 
-    if(url === '/') {
-        if(method === 'GET') {
-            response.statusCode = 200;
-            response.end('<h1>Ini adalah homepage</h1>');
-        } else {
-            response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
-        }
-    } 
-    
-    else if(url === '/about') {
-        // TODO 3: logika respons bila url bernilai '/about'
-        if(method === 'GET') {
-            response.statusCode = 200;
-            response.end('<h1>Halo! Ini adalah halaman about</h1>');
-        } else if(method === 'POST') {
-            let body = [];
+  const { method, url } = request;
 
-            request.on('data', (chunk) => {
-                body.push(chunk);
-            })
-
-            request.on('end', () => {
-                body = Buffer.concat(body).toString();
-                const { name } = JSON.parse(body);
-                response.statusCode = 200;
-                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
-            })
-        } else {
-            response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
-        }
+  if (url === "/") {
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end("<h1>Ini adalah homepage</h1>");
     } else {
-        response.statusCode = 404;
-        response.end('<h1>Halaman tidak ditemukan!</h1>');
+      response.statusCode = 400;
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
     }
- 
-    /** Kode komentar disembunyikan agar lebih ringkas */
+  } else if (url === "/about") {
+    // TODO 3: logika respons bila url bernilai '/about'
+    if (method === "GET") {
+      response.statusCode = 200;
+      response.end("<h1>Halo! Ini adalah halaman about</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-     // if(method === 'GET') {
-    //     response.end('<h1>Hello!</h1>');
-    // }
- 
-    // if(method === 'POST') {
-    //     let body = [];
-    
-    //     request.on('data', (chunk) => {
-    //         body.push(chunk);
-    //     });
- 
-    //     request.on('end', () => {
-    //         body = Buffer.concat(body).toString();
-    //         const { name } = JSON.parse(body);
-    //         response.end(`<h1>Hai, ${name}!</h2>`);
-    //     });
-    // }
-}
- 
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.statusCode = 200;
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      response.statusCode = 400;
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else {
+    response.statusCode = 404;
+    response.end("<h1>Halaman tidak ditemukan!</h1>");
+  }
+
+  /** Kode komentar disembunyikan agar lebih ringkas */
+
+  // if(method === 'GET') {
+  //     response.end('<h1>Hello!</h1>');
+  // }
+
+  // if(method === 'POST') {
+  //     let body = [];
+
+  //     request.on('data', (chunk) => {
+  //         body.push(chunk);
+  //     });
+
+  //     request.on('end', () => {
+  //         body = Buffer.concat(body).toString();
+  //         const { name } = JSON.parse(body);
+  //         response.end(`<h1>Hai, ${name}!</h2>`);
+  //     });
+  // }
+};
+
 const server = http.createServer(requestListener);
- 
+
 const port = 5000;
-const host = 'localhost';
- 
+const host = "localhost";
+
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
+  console.log(`Server berjalan pada http://${host}:${port}`);
 });
